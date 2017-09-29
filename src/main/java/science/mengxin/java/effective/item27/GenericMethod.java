b28abdcd6c7778be08afc2ce4b5d977ce86fef5e
Xin Meng <x.meng@outlook.com>
2017-09-29 09:04:51 +0100

    generic method

---
+++ src/main/java/science/mengxin/java/effective/item27/GenericMethod.java
@@ -0,0 +1,47 @@
+package science.mengxin.java.effective.item27;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * User:    mengxin
+ * Date:    29/09/2017
+ * Project: effective-java
+ * Package: science.mengxin.java.effective.item27
+ * Description: GenericMethod.
+ *
+ * @author mengxin
+ * @version 1.0
+ */
+public class GenericMethod {
+
+    // If don't use generic method, just could use the raw type
+    // Uses raw types - unacceptable! (Item 23)
+    // get warning:
+    // warning: [unchecked] unchecked call to
+    // HashSet(Collection<? extends E>) as a member of raw type HashSet
+    public static Set unionRayType(Set s1, Set s2) {
+        Set result = new HashSet(s1);
+        result.addAll(s2);
+        return result;
+    }
+
+    // Generic method
+    public static <E> Set<E> union(Set<E> s1, Set<E> s2) {
+
+        Set<E> result = new HashSet<E>(s1);
+
+        result.addAll(s2);
+
+        return result;
+    }
+
+    // Simple program to exercise generic method
+    public static void main(String[] args) {
+        Set<String> guys = new HashSet<String>(Arrays.asList("Tom", "Dick", "Harry"));
+        Set<String> stooges = new HashSet<String>(Arrays.asList("Larry", "Moe", "Curly"));
+        Set<String> aflCio = union(guys, stooges);
+        System.out.println(aflCio);
+    }
+}
