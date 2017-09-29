2c19536cd0fda7336b52bb4202cc78ce4e93e022
Xin Meng <x.meng@outlook.com>
2017-09-29 08:38:15 +0100

    without generic

---
+++ src/main/java/science/mengxin/java/effective/item26/Stack.java
@@ -0,0 +1,52 @@
+package science.mengxin.java.effective.item26;
+
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+/**
+ * User:    mengxin
+ * Date:    29/09/2017
+ * Project: effective-java
+ * Package: science.mengxin.java.effective.item26
+ * Description: Stack.
+ *
+ * @author mengxin
+ * @version 1.0
+ */
+// Object-based collection - a prime candidate for generics
+
+public class Stack {
+
+    private Object[] elements;
+    private int size = 0;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    public Stack() {
+        elements = new Object[DEFAULT_INITIAL_CAPACITY];
+    }
+
+    public void push(Object e) {
+        ensureCapacity();
+        elements[size++] = e;
+    }
+
+    public Object pop() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        Object result = elements[--size];
+        elements[size] = null; // Eliminate obsolete reference
+        return result;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private void ensureCapacity() {
+        if (elements.length == size) {
+            elements = Arrays.copyOf(elements, 2 * size + 1);
+        }
+    }
+
+}
