540b81332f21a801e663566caf89ffef3bfd45a3
Xin Meng <x.meng@outlook.com>
2017-10-01 11:53:13 +0100

    generic separate study

---
+++ src/test/java/science/mengxin/java/effective/generic/ArrayOfGenericReference.java
@@ -0,0 +1,15 @@
+package science.mengxin.java.effective.generic;
+
+/**
+ * User:    mengxin
+ * Date:    01/10/2017
+ * Project: effective-java
+ * Package: science.mengxin.java.effective.generic
+ * Description: ArrayOfGenericReference.
+ *
+ * @author mengxin
+ * @version 1.0
+ */
+public class ArrayOfGenericReference {
+    //static Generic<Integer>[] gia;
+}
